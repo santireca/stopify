f53d5ce0bd0280a6b901ec1da83719b6f19caa6c
santireca <santiagorecabarren7@gmail.com>
2023-08-14 15:46:51 -0300

    Stripe integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({ children } : {
 }) {
 
   const userSongs = await getSongsByUserId()
+
   return (
     <html lang="en">
       <body className={font.className}>
